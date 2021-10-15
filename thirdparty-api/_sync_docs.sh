ce493e8e6f89ae5cf4fe8c9777a67245f97f00fc
Lewis Daly <lewis@vesselstech.com>
2021-10-15 10:51:54 +0930

    feat: update swagger docs to latest version

--- thirdparty-api/_sync_docs.sh
+++ thirdparty-api/_sync_docs.sh
@@ -3,25 +3,21 @@
 ##
 # Synchronises the definition docs from their disparate locations into one place.
 #
-# This is a temporary measure to aid in writing and updating the proposed 3rd Party API
-# documentation.
-#
-# In the near future, this repository will be home to the PISP documentation, so we won't need
-# to have this script to sync everything
+# The API Spec for the Third Party API is managed by the api-snippets project
 ##
 
 set -eu
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PISP_PROJECT_GIT_URL="https://github.com/lewisdaly/pisp.git"
-PISP_PROJECT_BRANCH='feat/ccb-submission'
-CLONE_DIR='/tmp/pisp-project'
+GIT_URL="https://github.com/mojaloop/api-snippets.git"
+BRANCH='master'
+CLONE_DIR='/tmp/api-snippets'
 
 rm -rf ${CLONE_DIR}
 
-git clone -b ${PISP_PROJECT_BRANCH} ${PISP_PROJECT_GIT_URL} ${CLONE_DIR}
+git clone -b ${BRANCH} ${GIT_URL} ${CLONE_DIR}
 
 
 # API definition, grab from mojaloop/pisp-project
-cp ${CLONE_DIR}/src/interface/thirdparty-dfsp-api.yaml ${DIR}/thirdparty-dfsp-v1.0.yaml
-cp ${CLONE_DIR}/src/interface/thirdparty-pisp-api.yaml ${DIR}/thirdparty-pisp-v1.0.yaml
+cp ${CLONE_DIR}/thirdparty/openapi3/thirdparty-dfsp-api.yaml ${DIR}/thirdparty-dfsp-v1.0.yaml
+cp ${CLONE_DIR}/thirdparty/openapi3/thirdparty-pisp-api.yaml ${DIR}/thirdparty-pisp-v1.0.yaml
