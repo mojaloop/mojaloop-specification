e3429411eeaae9e73063b6ab429b14b821af7ce2
Lewis Daly <lewisdaly@me.com>
2021-10-20 16:07:34 +0930

    feat(openapi): update to latest thirdparty openapi spec

--- thirdparty-api/_sync_docs.sh
+++ thirdparty-api/_sync_docs.sh
@@ -17,7 +17,6 @@ rm -rf ${CLONE_DIR}
 
 git clone -b ${BRANCH} ${GIT_URL} ${CLONE_DIR}
 
-
 # API definition, grab from mojaloop/pisp-project
 cp ${CLONE_DIR}/thirdparty/openapi3/thirdparty-dfsp-api.yaml ${DIR}/thirdparty-dfsp-v1.0.yaml
 cp ${CLONE_DIR}/thirdparty/openapi3/thirdparty-pisp-api.yaml ${DIR}/thirdparty-pisp-v1.0.yaml
